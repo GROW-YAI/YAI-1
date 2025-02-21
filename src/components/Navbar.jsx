fe83ca7e693264ae723a7625d42cd261b838291f
Efuwa Akyere <efuwayorke13@gmail.com>
2025-02-21 18:12:37 +0000

    Setting up files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="p-6 flex justify-center align-middle bg-amber-200">
+    <div className="p-6 flex justify-center align-middle ">
       <h4 className="text-xl font-bold">Navbar</h4>
     </div>
   );
