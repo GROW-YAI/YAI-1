f58c450382ae049d1c794bf7cf0625321408c87b
Efuwa Akyere <efuwayorke13@gmail.com>
2025-02-26 18:56:51 +0000

    Completed the two navbars

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -1,11 +1,49 @@
 import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { FiFacebook, FiTwitter, FiPhoneCall } from "react-icons/fi";
+import { CiLinkedin, CiAt } from "react-icons/ci";
 
 const Navbar1 = () => {
   return (
-    <nav className="'bg-slate-100 text-black px-8 md:px-16 lg:px-24'">
-      <div></div>
-      <div></div>
-      <div></div>
+    <nav className="bg-[#191D34] text-white px-4 sm:px-8 md:px-16 lg:px-24">
+      <div className="container py-2 flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-2 sm:gap-0">
+        
+        <div className="flex flex-col sm:flex-row gap-2 sm:gap-6 items-center">
+          <div className="flex gap-2">
+            <span className="flex items-center text-[#FE5C24]">
+              <FiPhoneCall />
+            </span>
+            <span className="text-[.9rem]">+2332078763</span>
+          </div>
+
+          <div className="w-0.5 h-3 bg-slate-400 hidden sm:flex"></div>
+
+          <div className="flex gap-2">
+            <span className="flex items-center text-[#FE5C24]">
+              <CiAt size={20} />
+            </span>
+            <span className="text-[.9rem]">info@example.com</span>
+          </div>
+        </div>
+
+        <div className="mt-2 sm:mt-0">
+          <ul className="flex gap-2 cursor-pointer">
+            <li className="hover:text-[#FE5C24] transition-all duration-300">
+              <FiFacebook size={20} />
+            </li>
+            <li className="hover:text-[#FE5C24] transition-all duration-300">
+              <FaInstagram size={20} />
+            </li>
+            <li className="hover:text-[#FE5C24] transition-all duration-300">
+              <FiTwitter size={20} />
+            </li>
+            <li className="hover:text-[#FE5C24] transition-all duration-300">
+              <CiLinkedin size={25} />
+            </li>
+          </ul>
+        </div>
+
+      </div>
     </nav>
   );
 };
--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import logo from '../../assets/NoghusamLogo.jpg';
 
 const Navbar2 = () => {
   return (
-    <div>Navbar2</div>
-  )
-}
+    <nav className="bg-white pt-2 px-4 sm:px-8 md:px-16 lg:px-24">
+      <div className="container py-2 flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-4 sm:gap-0">
+        
+        <div className="flex gap-2 items-center">
+          <img src={logo} alt="logo" className="h-12 rounded-4xl" />
+          <h2 className="text-xl font-bold text-[#191D34]">NOGHUSAM</h2>
+        </div>
+        
+        <div>
+          <ul className="flex flex-col sm:flex-row gap-4 sm:gap-14 text-center">
+            <li className="hover:text-[#FE5C24] transition-all duration-300">
+              <a href="">Home</a>
+            </li>
+            <li className="hover:text-[#FE5C24] transition-all duration-300">
+              <a href="">Innovator</a>
+            </li>
+            <li className="hover:text-[#FE5C24] transition-all duration-300">
+              <a href="">Products</a>
+            </li>
+            <li className="hover:text-[#FE5C24] transition-all duration-300">
+              <a href="">Testimonials</a>
+            </li>
+          </ul>
+        </div>
+        
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar2
+export default Navbar2;
